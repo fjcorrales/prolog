655ff096cf89e19318b6adcd9fb6f4e55d70ce5e
fjcorrales <92602420+fjcorrales@users.noreply.github.com>
2022-04-18 21:09:08 +0200

    Update code.pl

--- code.pl
+++ code.pl
@@ -45,15 +45,76 @@ byte(BB):-
 byte(HB):-
     hex_byte(HB).
 
+
+%   PREDICADOS AUXILIARES
+
+/*
+    Para el Predicado 2 voy a necesitar una base de hechos, ya que mi intencion y objetivo es comparar si un hexadecimal
+    y un binario dados son equivalentes, para lo cual he de saber por que serie de 
+    binds que forman un bit representan un nibble hexadecimal.
+*/
+
+hex_to_bin_byte([hexd(0)], [bind(0),bind(0),bind(0),bind(0)]). %el 0
+hex_to_bin_byte([hexd(1)], [bind(0),bind(0),bind(0),bind(1)]). %el 1
+hex_to_bin_byte([hexd(2)], [bind(0),bind(0),bind(1),bind(0)]). %el 2
+hex_to_bin_byte([hexd(3)], [bind(0),bind(0),bind(1),bind(1)]). %el 3
+hex_to_bin_byte([hexd(4)], [bind(0),bind(1),bind(0),bind(0)]). %el 4
+hex_to_bin_byte([hexd(5)], [bind(0),bind(1),bind(0),bind(1)]). %el 5
+hex_to_bin_byte([hexd(6)], [bind(0),bind(1),bind(1),bind(0)]). %el 6
+hex_to_bin_byte([hexd(7)], [bind(0),bind(1),bind(1),bind(1)]). %el 7
+hex_to_bin_byte([hexd(8)], [bind(1),bind(0),bind(0),bind(0)]). %el 8
+hex_to_bin_byte([hexd(9)], [bind(1),bind(0),bind(0),bind(1)]). %el 9
+hex_to_bin_byte([hexd(a)], [bind(1),bind(0),bind(1),bind(0)]). %el 10
+hex_to_bin_byte([hexd(b)], [bind(1),bind(0),bind(1),bind(1)]). %el 11
+hex_to_bin_byte([hexd(c)], [bind(1),bind(1),bind(0),bind(0)]). %el 12
+hex_to_bin_byte([hexd(d)], [bind(1),bind(1),bind(0),bind(1)]). %el 13
+hex_to_bin_byte([hexd(e)], [bind(1),bind(1),bind(1),bind(0)]). %el 14
+hex_to_bin_byte([hexd(f)], [bind(1),bind(1),bind(1),bind(1)]). %el 15
+
+
 %Predicado 1 -> byte_list/1
 /*
     Lo que queremos es comprobar si los elementos de una lista dada son de tipo byte, por lo que una funcion
     recursiva es una buena opcion, ya que asi, lo que puedo hacer es comprobar cada elemento de manera individual 
     llamando a mi predicado byte/1 
 */
-byte_list([]).
+byte_list([]).                                  %caso base
 byte_list([B|L]):-
     byte(B),
     byte_list(L).
 
 %Predicado 2 -> byte_conversion/2
+/*
+    En este predicado, mi objetivo es comprobar si un hexadecimal y un binario dados representan el mismo numero
+    para esto lo que voy a hacer es comprobar si el primer nibble del byte esta represetnado por los primeros 4 bind
+    de la cadena de 8 bits y el segundo nibble con los otros 4 siguientes. Esto gracias a la base de hechos que he conformado
+    mas arriba
+*/
+byte_conversion([H1,H0], [B7,B6,B5,B4,B3,B2,B1,B0]):-
+    hex_byte([H1,H0]),                          %compruebo si H1-H0 representan un byte hexadecimal
+    binary_byte([B7,B6,B5,B4,B3,B2,B1,B0]),     %compruebo si B7-B0 representan un byte binario
+    hex_to_bin_byte([H1],[B7,B6,B5,B4]),        %compruebo si son equivalentes las respectivas mitades
+    hex_to_bin_byte([H0],[B3,B2,B1,B0]).
+
+%Predicado 3 -> byte_list_conversion/2
+/*
+    En este predicado, mi objetivo es comparar si dos listas de bytes, una hex y otra bin, representan lo mismo
+    este predicado hara uso del predicado 1 ya que este lo que hace es compararme un hex y un bin,
+    las llamadas al mismo se realizaran con los primeros elementos de cada lista. Ademas este predicado 
+    sera recursivo para asi poder ir vaciando las listas dadas y así poder comprobar todos los elementos.
+*/
+byte_list_conversion([],[]).                    %caso base
+byte_list_conversion([H|Hn],[B|Bn]):-
+    byte_conversion(H,B),
+    byte_list_conversion(Hn,Bn).
+
+%Predicado 4 -> get_nth_bit_from_byte/3
+
+
+%Predicado 5 -> byte_list_clsh/2
+
+
+%Predicado 6 -> byte_list_crsh/2
+
+
+%Predicado 7 -> byte_xor/3
